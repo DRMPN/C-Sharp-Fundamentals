6da2df2933b841ab583de9ba4144a774158c67fb
ilyushka <61294398+DRMPN@users.noreply.github.com>
2024-02-04 20:51:14 +0300

    Add assignment 1

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write any number: ");
+            int abc = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You wrote number: {0}", abc);
+            Console.ReadKey();
+        }
+    }
+}
