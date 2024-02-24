55f52139bf3d42a3cbe126430da7972b66d54fdc
ilyushka <mr.gorden.smit@gmail.com>
2024-02-25 00:27:47 +0300

    Add assignment 10

---
+++ ClassesAndObjects/Angle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ClassesAndObjects
+{
+    internal class Angle
+    {
+        int degree;
+        int minute;
+        int second;
+
+
+        public int Degree
+        {
+            get { return degree; }
+            set { degree = value % 360; }
+        }
+
+        public int Minute
+        {
+            get { return minute; }
+            set { minute = value % 60; }
+        }
+
+        public int Second
+        {
+            get { return second; }
+            set { second = value % 60; }
+        }
+
+        public Angle(int degrees, int minutes, int seconds)
+        {
+            Degree = degrees;
+            Minute = minutes;
+            Second = seconds;
+        }
+
+        public double ToRadians()
+        {
+            double decimalDegrees = degree + (minute + second / 60) / 60;
+            return decimalDegrees * Math.PI / 180;
+        }
+
+    }
+}
---
+++ ClassesAndObjects/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndObjects
+{
+    class Program
+    {
+        // Угол задан с помощью целочисленных значений gradus - градусов, min - угловых минут, sec - угловых секунд.
+        // Реализовать класс, в котором указанные значения представлены в виде свойств.
+        // Для свойств предусмотреть проверку корректности данных.
+        // Класс должен содержать конструктор для установки начальных значений, а также метод ToRadians для перевода угла в радианы.
+        // Создать объект на основе разработанного класса.
+        // Осуществить использование объекта в программе.
+        static void Main(string[] args)
+        {
+            Angle angle = new Angle(55, 45, 0);
+            Console.WriteLine(angle.ToRadians());
+            Console.ReadKey();
+        }
+    }
+}
