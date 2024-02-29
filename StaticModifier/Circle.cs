158e0576a1648b9106da2499c0665e922bfc4e5a
ilyushka <mr.gorden.smit@gmail.com>
2024-03-01 00:08:56 +0300

    Add assignment 12

---
+++ StaticModifier/Circle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace StaticModifier
+{
+	public static class Circle
+	{
+		public static double Circumference(double r)
+        {
+			return 2 * Math.PI * r;
+        }
+
+		public static double Area(double r)
+        {
+			return Math.PI * Math.Pow(r, 2);
+		}
+
+		public static bool IsPointInCircle(double x, double y, double r, double centerX, double centerY)
+        {
+			return Math.Pow((x - centerX),2) + Math.Pow((y - centerY), 2) < r * r;
+        }
+	}
+}
---
+++ StaticModifier/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticModifier
+{
+    class Program
+    {
+        // Разработать статический класс для работы с окружностью. Класс должен содержать 3 метода:
+        // метод, определяющий длину окружности по заданному радиусу;
+        // метод, определяющий площадь круга по заданному радиусу;
+        // метод, проверяющий принадлежность точки с координатами (x,y) кругу с радиусом r и координатами центра x0, y0.
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Circle.Circumference(3));
+            Console.WriteLine(Circle.Area(3));
+            Console.WriteLine(Circle.IsPointInCircle(1,1,2,0,0));
+            Console.WriteLine(Circle.IsPointInCircle(5,5,4,0,0));
+            Console.ReadKey();
+        }
+    }
+}
