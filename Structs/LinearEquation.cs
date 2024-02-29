392809b37867f7ce85733fdcf358b9b45a62ddcf
ilyushka <mr.gorden.smit@gmail.com>
2024-02-29 23:13:14 +0300

    Add assignment 11

---
+++ Structs/LinearEquation.cs
@@ -0,0 +1,19 @@
+﻿namespace Structs
+{
+    internal struct LinearEquation
+    {
+        double k;
+        double b;
+
+        public LinearEquation(double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+
+        public double Root()
+        {
+            return -this.b / this.k;
+        }
+    }
+}
---
+++ Structs/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs
+{
+    class Program
+    {
+        // Разработать структуру для решения линейного уравнения 0=kx+b.
+        // Коэффициенты уравнения k, b реализовать с помощью полей вещественного типа.
+        // Для решения уравнения предусмотреть метод Root.
+        // Создать экземпляр разработанной структуры.
+        // Осуществить использование экземпляра в программе.
+        static void Main(string[] args)
+        {
+            LinearEquation le = new LinearEquation(5, 4);
+            Console.WriteLine(le.Root());
+            Console.ReadKey();
+        }
+    }
+}
