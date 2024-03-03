411166b16d3efda48e69d69895cec43a32b4e67d
ilyushka <mr.gorden.smit@gmail.com>
2024-03-03 05:17:46 +0300

    Add assignment 13

---
+++ Inheritance/Building.cs
@@ -0,0 +1,23 @@
+﻿namespace Inheritance
+{
+    class Building
+    {
+        string Address { get; set; }
+        double Length { get; set; }
+        double Width { get; set; }
+        double Height { get; set; }
+
+        public Building(string address, double length, double width, double height)
+        {
+            Address = address;
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+        public string Print()
+        {
+            return $"Address: {Address}, L: {Length}, W: {Width}, H: {Height}";
+        }
+    }
+}
---
+++ Inheritance/MultiBuilding.cs
@@ -0,0 +1,17 @@
+﻿namespace Inheritance
+{
+    sealed class MultiBuilding:Building
+    {
+        int Floors { get; set; }
+
+        public MultiBuilding(string address, double length, double width, double height, int floors) :base(address, length, width, height)
+        {
+            Floors = floors;
+        }
+
+        public string Print()
+        {
+            return base.Print() + $", Floors: {Floors}";
+        }
+    }
+}
